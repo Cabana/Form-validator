b1b71751856ca9b22220ec434e198a328028c85e
David Pedersen <david.pdrsn@gmail.com>
2013-08-30 13:04:46 +0200

    update demo
    demo isn't actually a real demo, its more a place where I do smoke
    testing

--- demo/script.js
+++ demo/script.js
@@ -1,41 +1,29 @@
+var validator = new FormValidator();
+
+validator.defineValidation('required', function(input, data){
+  var errors = [];
+  var errorMessage = "Can't be blank";
+
+  if (input.nodeName.toLowerCase() == 'select') {
+    var text = input.querySelector('option').innerText;
+    var value = input.value;
+
+    if (text == value) {
+      errors.push(errorMessage);
+    }
+  }
+
+  if (!/.+/.test(input.value)) {
+    errors.push(errorMessage);
+  }
+
+  return errors;
+});
+
 $(function(){
 
-  var customValidator = new FormValidator();
-
-  customValidator.defineValidation('cpr', /\d{6}-\d{4}/, 'Invalid cpr number');
-
-  customValidator.defineValidation('zip', /^\d{4}$/, 'Invalid zip code');
-
-  customValidator.defineValidation('newRequired', function(input, data) {
-    if (!/^.+$/.test(input.value)) {
-      return "Can't be blank";
-    }
-  });
-
-  customValidator.defineValidation('exactLength', function(input, data) {
-    if (input.value.length != data.exactLength) {
-      return "Input must be exactly " + data.exactLength + " characters long";
-    }
-  });
-
-  $('form').validate({
-    validator: customValidator,
-
-    onBlur: true,
-
-    beforeValidation: function() {
-    },
-
-    afterValidation: function() {
-    },
-
-    ifValid: function() {
-      console.log('valid');
-    },
-
-    ifInvalid: function() {
-      console.log('invalid');
-    }
+  $('#mainform').validate({
+    validator: validator
   });
 
 });
