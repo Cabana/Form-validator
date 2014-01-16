0e0119aecb5e892a5b744aaa1c94d0aeaf1b8000
David Pedersen <david.pdrsn@gmail.com>
2014-01-16 14:00:51 +0100

    change demos for doing smoke tests

--- demo/script.js
+++ demo/script.js
@@ -2,6 +2,10 @@ $(function(){
 
   var validator = new FormValidator();
 
+  var input = document.getElementById("foo");
+  console.log(validator.validateInput(input));
+  console.log(input.value);
+
   $("#mainform").validate({
     validator: validator
   });
