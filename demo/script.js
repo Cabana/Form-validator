10ea616acff709b9e79f8712d15e9a4859d451c2
David Pedersen <david.pdrsn@gmail.com>
2014-02-05 12:56:37 +0100

    update demo

--- demo/script.js
+++ demo/script.js
@@ -1,16 +1,7 @@
 $(function(){
 
   $("#mainform").validate({
-    onBlur: true,
-
-    ifInvalid: function() {
-      alert('invalid');
-    },
-
-    ifValid: function(e) {
-      alert('valid');
-      e.preventDefault();
-    }
+    onBlur: true
   });
 
 });
